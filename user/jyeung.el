568c752899ca7fd185ed17a99f51d4fd7dd9b88b
Jake Yeung <jakeyeung@gmail.com>
2014-10-24 17:00:28 +0200

    init commit

---
+++ user/jyeung.el
@@ -0,0 +1,16 @@
+(setq org-agenda-files (list "~/orgmode/org-jake/projects.org"
+			     "~/orgmode/org-jake/next-actions.org"
+			     "~/orgmode/org-jake/someday.org"))
+(setq org-clock-idle-time '15)
+
+(setq user-full-name "Jake Yeung"
+      googleid "jakeyeung"
+      user-mail-address "jakeyeung@gmail.com"
+      ;; specify how email is sent
+      send-mail-function 'smtpmail-send-it
+      ;; used in message mode
+      message-send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-server "mail.google.com"
+      smtpmail-smtp-service 587)
+
+(setq column-number-mode t)
