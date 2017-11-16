abc0537ae003189810bdc2c3d38b4803d7ec5329
Jake Yeung <jakeyeung@gmail.com>
2017-11-16 14:01:25 +0100

    Update init.el to enhance orgmode with beamer and screenshot

--- init.el
+++ init.el
@@ -6,10 +6,14 @@
 
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+;; Add agenda https://stackoverflow.com/questions/44094125/org-mode-c-c-a-undefined
+(global-set-key "\C-ca" 'org-agenda)
+
 (setq org-agenda-files (list "~/orgmode/org-jake/projects.org"
 			     "~/orgmode/org-jake/next-actions.org"
 			     "~/orgmode/org-jake/someday.org"
-				 "~/orgmode/org-jake/timekeeping.org"))
+				 "~/orgmode/org-jake/timekeeping.org"
+				 "~/.deft"))
 (setq org-default-notes-file "~/orgmode/org-jake/notes.org")
 (setq org-clock-idle-time '15)
 (setq org-archive-location "~/orgmode/org-jake/archive/%s_archive::* Archived Tasks")
@@ -91,9 +95,81 @@ same directory as the org-buffer and insert a link to this file."
   (if (file-exists-p filename)
     (insert (concat "[[file:" filename "]]"))))
 
-(global-set-key "\C-x\C-t" 'my-org-screenshot)
+(global-set-key (kbd "C-c 4") 'my-org-screenshot)
 
 
 ;; Open timekeeping.org with hotkey
 (global-set-key (kbd "C-c o") 
                 (lambda () (interactive) (find-file "~/orgmode/org-jake/timekeeping.org")))
+
+;; Make latex
+(global-set-key "\C-x\C-l" 'org-beamer-export-to-latex)
+
+
+;; http://emacs-fu.blogspot.ch/2009/10/writing-presentations-with-org-mode-and.html
+;;;;;; Add beamer boiler plate
+;; allow for export=>beamer by placing
+;; #+LaTeX_CLASS: beamer in org files
+;; (unless (boundp 'org-export-latex-classes)
+(unless (boundp 'org-latex-classes)
+  (setq org-export-latex-classes nil))
+(add-to-list 'org-export-latex-classes
+;;(add-to-list 'org-latex-classes
+  ;; beamer class, for presentations
+  '("beamer"
+     "\\documentclass[11pt]{beamer}\n
+      \\mode<{{{beamermode}}}>\n
+      \\usetheme{{{{beamertheme}}}}\n
+      \\usecolortheme{{{{beamercolortheme}}}}\n
+      \\beamertemplateballitem\n
+      \\setbeameroption{show notes}
+      \\usepackage[utf8]{inputenc}\n
+      \\usepackage[T1]{fontenc}\n
+      \\usepackage{hyperref}\n
+      \\usepackage{color}
+      \\usepackage{listings}
+      \\lstset{numbers=none,language=[ISO]C++,tabsize=4,
+  frame=single,
+  basicstyle=\\small,
+  showspaces=false,showstringspaces=false,
+  showtabs=false,
+  keywordstyle=\\color{blue}\\bfseries,
+  commentstyle=\\color{red},
+  }\n
+      \\usepackage{verbatim}\n
+      \\institute{{{{beamerinstitute}}}}\n          
+       \\subject{{{{beamersubject}}}}\n"
+
+     ("\\section{%s}" . "\\section*{%s}")
+     
+     ("\\begin{frame}[fragile]\\frametitle{%s}"
+       "\\end{frame}"
+       "\\begin{frame}[fragile]\\frametitle{%s}"
+       "\\end{frame}")))
+
+  ;; letter class, for formal letters
+
+  (add-to-list 'org-export-latex-classes
+  ;; (add-to-list 'org-latex-classes
+
+  '("letter"
+     "\\documentclass[11pt]{letter}\n
+      \\usepackage[utf8]{inputenc}\n
+      \\usepackage[T1]{fontenc}\n
+      \\usepackage{color}"
+     
+     ("\\section{%s}" . "\\section*{%s}")
+     ("\\subsection{%s}" . "\\subsection*{%s}")
+     ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+     ("\\paragraph{%s}" . "\\paragraph*{%s}")
+     ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+;; Set environment for latex
+(setenv "PATH" (concat (getenv "PATH") ":/Library/TeX/texbin"))
+
+;; Focus follows mouse
+(setq mouse-autoselect-window t)
+
+;; Calendar views TODOs are added
+(setq org-icalendar-use-deadline '(event-if-not-todo event-if-todo todo-due))
+(setq org-icalendar-use-scheduled '(event-if-not-todo event-if-todo todo-due))
